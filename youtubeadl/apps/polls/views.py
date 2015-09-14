7ecfa7065472fa4a4f5fc2cb3ffc87d0951accdc
simranjit sachar <simranjitsachar@simranjits-MacBook-Air.local>
2015-09-14 19:59:00 -0400

    changes

---
+++ youtubeadl/apps/polls/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Question
+
+from django.shortcuts import get_object_or_404, render
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
--- youtubeadl/settings/base.py
+++ youtubeadl/settings/base.py
@@ -31,23 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    # Grappelli needs to be defined before the admin.
-    'grappelli',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # 3rd-party apps.
-    'compressor',
-    'django_extensions',
-
-    # Project apps.
-    'youtubeadl.apps.core',
-    'youtubeadl.apps.downloader',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,22 +56,14 @@ ROOT_URLCONF = 'youtubeadl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                # Local context processors.
-                'youtubeadl.apps.core.context_processors.third_party_tracking_ids',
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
             ],
         },
     },
@@ -95,12 +77,8 @@ WSGI_APPLICATION = 'youtubeadl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DATABASE_NAME', 'youtubeadl'),
-        'USER': os.getenv('DATABASE_USER', 'youtubeadl'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DATABASE_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -130,108 +108,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-
-# Media files.
-
-MEDIA_ROOT = os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
-
-MEDIA_URL = '/media/'
-
-
-# Email settings.
-
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-
-DEFAULT_FROM_EMAIL = 'Info <info@youtubeadl.com>'
-SERVER_EMAIL = 'Alerts <alerts@youtubeadl.com>'
-
-ADMINS = (
-    ('Admin', 'admin@youtubeadl.com'),
-)
-
-
-# Logging settings.
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'default': {
-            'format': '%(asctime)s  [%(name)s:%(lineno)s]  %(levelname)s - %(message)s',
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s',
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'default',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler',
-        }
-    },
-    'loggers': {
-        # Silence SuspiciousOperation.DisallowedHost exception ('Invalid
-        # HTTP_HOST' header messages). Set the handler to 'null' so we don't
-        # get those annoying emails.
-        'django.security.DisallowedHost': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        '': {
-            'handlers': ['console', ],
-            'level': 'INFO',
-        }
-    }
-}
-
-
-# YouTube Audio Downloader settings.
-MAX_DURATION_SECONDS = 10800
-DAILY_CONVERT_LIMIT = 25
-
-
-# 3rd-party apps tracking IDs.
-GOOGLE_ANALYTICS_TRACKING_ID = os.getenv('GOOGLE_ANALYTICS_TRACKING_ID')
-ADDTHIS_PUBLISHER_ID = os.getenv('ADDTHIS_PUBLISHER_ID')
-
-
-# Celery settings.
-BROKER_URL = os.getenv('BROKER_URL', 'amqp://guest:guest@127.0.0.1//')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'amqp')
-
-
-# Grappelli settings.
-GRAPPELLI_ADMIN_TITLE = 'YouTube ADL Admin'
