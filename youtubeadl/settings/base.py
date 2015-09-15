eec25b48a37b61ad17e18f021be0912e03ef486e
simranjit sachar <simranjitsachar@simranjits-MacBook-Air.local>
2015-09-14 21:46:52 -0400

    update

--- youtubeadl/settings/base.py
+++ youtubeadl/settings/base.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'youtubeadl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME', 'youtubeadl'),
+        'USER': os.getenv('DATABASE_USER', 'youtubeadl'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
 
